dff5a291d0875fa2bb96b9a4e4237f4782861a7c
Andrea Magni <andrea.magni@issgreppi.it>
2023-10-30 10:50:48 +0100

    finito es1

--- Attività1/Arduino_es1.ino/Arduino_es1.ino.ino
+++ Attività1/Arduino_es1.ino/Arduino_es1.ino.ino
@@ -3,13 +3,16 @@ void setup()
   //configurazione pin 
   pinMode(10, OUTPUT);
   pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-   
+  pinMode(9, OUTPUT); 
 }
 
 void loop() 
 {
-  digitalWrite(10, HIGH);
   digitalWrite(8, HIGH);
   digitalWrite(9, LOW);
+  for(int i = 0; i < 255; i++)
+  {
+    analogWrite(10, i);
+    delay(100);
+  }  
 }
