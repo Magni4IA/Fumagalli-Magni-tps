7a05b65723661812e258f7e90c9089a0b98f9713
Andrea Magni <113580464+Magni4IA@users.noreply.github.com>
2024-04-08 10:55:52 +0200

    Add files via upload

---
+++ Attività 7)/Fase2/ricavaStringa.py
@@ -0,0 +1,18 @@
+# content = "ciao"
+# #scrivo nel file e poi lo chiudo
+# file1 = open("fase2.txt", "w") #w-> write
+# file1.write(content)
+# file1.close()
+
+# content = "\naddio"
+# #aggiungo content2 nel file e poi lo chiudo
+# file1 = open("fase2.txt", "a") #a-> append 
+# file1.write(content)
+# file1.close()
+
+# #ottengo una lista di stringhe che sono le righe del file
+# var_lettura = open("fase2.txt", "r").readlines() #r-> read
+# print(var_lettura)ù
+
+#ESERCIZIO 
+lista = open()
